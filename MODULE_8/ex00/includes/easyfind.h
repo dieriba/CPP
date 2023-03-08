d37924a300401a2c9fb1682fed0667c359a2c814
Dieriba TOURE <dtoure@e2r7p14.clusters.42paris.fr>
2023-03-08 19:32:57 +0100

    shell

--- MODULE_8/ex00/includes/easyfind.h
+++ MODULE_8/ex00/includes/easyfind.h
@@ -5,12 +5,12 @@
 #include <algorithm>
 #include <iostream>
 template<template <typename> class T>
-typename T<int>::iterator& easyfind(T<int> &container, int to_find)
+typename T<int>::iterator easyfind(T<int> &container, int to_find)
 {
-    return std::find(container.begin(), container.end(), to_find);    
+    return std::find(container.begin(), container.end(), to_find);
 }
 template<template <typename,typename> class T, typename Alloc>
-typename T<int, Alloc>::iterator& easyfind(T<int, Alloc> &container, int to_find)
+typename T<int, Alloc>::iterator easyfind(T<int, Alloc> &container, int to_find)
 {
     return std::find(container.begin(), container.end(), to_find);    
 }
