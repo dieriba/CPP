b4799280bfe4fbbc2434b2f7c1b42d07e5dccbf1
dieriba <dieriba.pro@gmail.com>
2023-03-24 13:18:42 +0100

    clean ex01

--- MODULE_0/ex01/contact.cpp
+++ MODULE_0/ex01/contact.cpp
@@ -2,68 +2,25 @@
 
 void Contact::show_info(void)
 {
-	std::cout << "index: " << this -> _index << std::endl;
-	std::cout << "last_name: " << this -> _last_name << std::endl;
-	std::cout << "first_name: " << this -> _first_name << std::endl;
-	std::cout << "phone_number: " << this -> _phone_number << std::endl;
-	std::cout << "darkset_secret: " << this -> _darkest_secret << std::endl;
+	std::cout << "index: " << _index << std::endl;
+	std::cout << "last_name: " << _last_name << std::endl;
+	std::cout << "first_name: " << _first_name << std::endl;
+	std::cout << "phone_number: " << _phone_number << std::endl;
+	std::cout << "darkset_secret: " << _darkest_secret << std::endl;
 }
-
-void Contact::set_index(int index)
-{
-	this -> _index = index;
-}
-
-void Contact::set_last_name(std::string& _last_name)
-{
-	this -> _last_name = _last_name;
-}
-
-void Contact::set_first_name(std::string& _first_name)
-{
-	this -> _first_name = _first_name;
-}
-
-void Contact::set_nick_name(std::string& _nick_name)
-{
-	this -> _nick_name = _nick_name;
-}
-
-void Contact::set_phone_number(std::string& _phone_number)
-{
-	this -> _phone_number = _phone_number;
-}
-
-void Contact::set_darkest_secret(std::string& _darkest_secret)
-{
-	this -> _darkest_secret = _darkest_secret;
-}
-
-std::string Contact::get_last_name(void)
-{
-	return (this -> _last_name);
-}
-
-std::string Contact::get_first_name(void)
-{
-	return (this -> _first_name);
-}
-
-std::string Contact::get_nickname(void)
-{
-	return (this -> _nick_name);
-}
-
-std::string Contact::get_darkset_secret(void)
-{
-	return (this -> _darkest_secret);
-}
-
-std::string Contact::get_phone_number(void)
-{
-	return (this -> _phone_number);
-}
-int	Contact::get_index(void)
-{
-	return (this -> _index);
-}
+/*--------------------------------------------------SETTER---------------------------------------------*/
+void Contact::set_index(const int& index){_index = index;}
+void Contact::set_last_name(const std::string& last_name){_last_name = last_name;}
+void Contact::set_first_name(const std::string& first_name){_first_name = first_name;}
+void Contact::set_nick_name(const std::string& nick_name){_nick_name = nick_name;}
+void Contact::set_phone_number(const std::string& phone_number){_phone_number = phone_number;}
+void Contact::set_darkest_secret(const std::string& darkest_secret){_darkest_secret = darkest_secret;}
+/*--------------------------------------------------SETTER---------------------------------------------*/
+/*--------------------------------------------------GETTER---------------------------------------------*/
+const std::string& Contact::get_last_name(void) const {return _last_name;}
+const std::string& Contact::get_first_name(void) const {return _first_name;}
+const std::string& Contact::get_nickname(void) const {return _nick_name;}
+const std::string& Contact::get_darkset_secret(void) const {return _darkest_secret;}
+const std::string& Contact::get_phone_number(void) const {return _phone_number;}
+const int&	Contact::get_index(void) const {return _index;}
+/*--------------------------------------------------GETTER---------------------------------------------*/
--- MODULE_0/ex01/includes/contact.hpp
+++ MODULE_0/ex01/includes/contact.hpp
@@ -5,19 +5,21 @@
 class Contact
 {
 	public:
-		void	set_index(int index);
-		void	set_last_name(std::string& _last_name);
-		void	set_first_name(std::string& _first_name);
-		void	set_nick_name(std::string& _nick_name);
-		void	set_phone_number(std::string& _phone_number);
-		void	set_darkest_secret(std::string& _darkest_secret);
+		void	set_index(const int& index);
+		void	set_last_name(const std::string& _last_name);
+		void	set_first_name(const std::string& _first_name);
+		void	set_nick_name(const std::string& _nick_name);
+		void	set_phone_number(const std::string& _phone_number);
+		void	set_darkest_secret(const std::string& _darkest_secret);
 		void	show_info();
-		std::string	get_last_name(void);
-		std::string	get_first_name(void);
-		std::string	get_nickname(void);
-		std::string	get_darkset_secret(void);
-		std::string	get_phone_number(void);
-		int			get_index(void);
+		
+		/*GETTER*/
+		const std::string&	get_last_name(void) const;
+		const std::string&	get_first_name(void) const;
+		const std::string&	get_nickname(void) const ;
+		const std::string&	get_darkset_secret(void) const;
+		const std::string&	get_phone_number(void) const;
+		const int&			get_index(void) const;
 
 	private:
 		int			_index;
